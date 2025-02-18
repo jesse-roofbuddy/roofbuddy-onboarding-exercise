3167fb57d305952b15094f79dcccc16a07bad202
Jesse Caunter <jesse@unisphere.co.nz>
2025-02-18 14:23:30 +1300

    fix: Clear database before seeding to avoid duplicate data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.menuItem.deleteMany({});
+
   await prisma.menuItem.createMany({
     data: [
       {
